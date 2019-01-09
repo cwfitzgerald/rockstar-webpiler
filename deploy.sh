fd50bc0d104773ecfbfd28d590e5a47fbcfd9e40
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-01-09 02:20:25 -0500

    Clean up deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 rm -r bundle
 mkdir -p bundle/static/js
 cp -r backend/{static/,templates/,*.py,requirements.txt} bundle/
-sed '/sourceMappingURL/d' js/target/scala-2.12/{rockstar-jsdeps.min.js,rockstar-opt.js} | tr -d '\n' > bundle/static/js/rockstar.js
+sbt fullOptJS
+sed -i '/sourceMappingURL/d' bundle/static/js/rs.js bundle/static/bootstrap.min.css
 rsync -az --progress --exclude venv --exclude __pycache__ bundle/ rockstar.connorwfitzgerald.com:/mnt/volume-nyc1-webhost/webroot/rockstar.connorwfitzgerald.com/ --delete
-ssh -t rockstar.connorwfitzgerald.com 'cd /mnt/volume-nyc1-webhost/webroot/rockstar.connorwfitzgerald.com/; source venv/bin/activate; pip install -r requirements.txt; deactivate; sudo supervisorctl restart rockstar'
+ssh -t rockstar.connorwfitzgerald.com "bash -c 'cd /mnt/volume-nyc1-webhost/webroot/rockstar.connorwfitzgerald.com/; source venv/bin/activate; pip install -r requirements.txt; deactivate; sudo chmod -R o+rX .; sudo supervisorctl restart rockstar'"
