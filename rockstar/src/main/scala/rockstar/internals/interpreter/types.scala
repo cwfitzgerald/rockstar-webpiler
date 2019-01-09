fccee4d0d363db72a787bf80012b1d8e75f21e59
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-01-09 18:40:26 -0500

    Add runtime typenames to be extracted

--- rockstar/src/main/scala/rockstar/internals/interpreter/types.scala
+++ rockstar/src/main/scala/rockstar/internals/interpreter/types.scala
@@ -3,22 +3,36 @@ package rockstar.internals.interpreter
 import rockstar.internals.ast
 
 object types {
-	sealed trait RuntimeValue
-	sealed case class RuntimeMysterious() extends RuntimeValue
-	sealed case class RuntimeNull() extends RuntimeValue
+	sealed trait RuntimeValue {
+		val typename: String
+	}
+	sealed case class RuntimeMysterious() extends RuntimeValue {
+		val typename: String = "mysterious"
+	}
+	sealed case class RuntimeNull() extends RuntimeValue {
+		val typename: String = "null"
+	}
 	sealed trait RuntimeContainerValue[T] { val value: T }
 	sealed case class RuntimeBoolean(value: Boolean)
 		extends RuntimeContainerValue[Boolean]
-			with RuntimeValue
+			with RuntimeValue {
+		val typename: String = "boolean"
+	}
 	sealed case class RuntimeNumber(value: Double)
 		extends RuntimeContainerValue[Double]
-			with RuntimeValue
+			with RuntimeValue {
+		val typename: String = "number"
+	}
 	sealed case class RuntimeString(value: String)
 		extends RuntimeContainerValue[String]
-			with RuntimeValue
+			with RuntimeValue {
+		val typename: String = "string"
+	}
 	sealed case class RuntimeFunction(value: ast.FunctionStatement)
 		extends RuntimeContainerValue[ast.FunctionStatement]
-			with RuntimeValue
+			with RuntimeValue {
+		val typename: String = "function"
+	}
 
 	object exceptions {
 		sealed trait ControlFlowException {}
